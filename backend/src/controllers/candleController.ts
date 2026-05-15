31d2aa0abc1b9f53a3bd946d0e3e70a25f1b68b9
Wesley <wesquirk@gmail.com>
2026-05-14 23:26:44 -0700

    added error handling for getCandles

--- backend/src/controllers/candleController.ts
+++ backend/src/controllers/candleController.ts
@@ -20,9 +20,17 @@ export async function getCandles(req: Request, res: Response) {
 
   const candleQuery: CandleQuery = req.body.candleQuery;
 
+  const candleName = candleQuery.candleName;
+
+  const candleStyle = candleQuery.candleStyle;
+
   const fragranceArray = candleQuery.fragrances;
 
-  console.log(fragranceArray);
+  const where: any = {};
+
+  if (fragranceArray && fragranceArray.length > 0) {
+    where.AND = generateAND(fragranceArray);
+  }
 
   function generateAND(fragranceArray: string[]) {
     const AND: object[] = [];
@@ -41,16 +49,38 @@ export async function getCandles(req: Request, res: Response) {
     return AND;
   }
 
-  const findCandles = await prisma.candles.findMany({
-    relationLoadStrategy: "join",
-    where: {
-      AND: generateAND(fragranceArray),
-    },
-    select: {
-      candle_name: true,
-      candle_style: true,
-    },
-  });
+  try {
+    const findCandles = await prisma.candles.findMany({
+      relationLoadStrategy: "join",
+      where: where,
+      select: {
+        candle_name: true,
+        candle_style: true,
+      },
+    });
 
-  return res.json({ query: findCandles });
+    if (findCandles.length == 0) {
+      return res.status(200).json({ noresults: "No Results Found" });
+    }
+
+    return res.json({ query: findCandles });
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === "PrismaClientKnownRequestError") {
+        return res.status(400).json({
+          error: error.message,
+        });
+      }
+
+      if (error.name === "PrismaClientValidationError") {
+        return res.status(500).json({
+          error: error.message,
+        });
+      }
+    }
+
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
 }
