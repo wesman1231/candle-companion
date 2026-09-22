f88061ef736acecc9e05073708e8544fd6288525
Wesley <wesley@DESKTOP-KRIS84H.localdomain>
2026-09-22 00:28:24 -0700

    updated git ignore

--- backend/src/scrapers/yankeeScrape.py
+++ backend/src/scrapers/yankeeScrape.py
@@ -1,58 +1,68 @@
+import os
 from playwright.sync_api import sync_playwright
 from seleniumbase import sb_cdp
-import re
+import psycopg
+
+dbName = os.getenv("dbName")
+dbUser = os.getenv("dbUser")
+dbPassword = os.getenv("dbPassword")
 
 sb = sb_cdp.Chrome()
 endpoint_url = sb.get_endpoint_url()
+with psycopg.connect(f"dbname={dbName} user={dbUser} password={dbPassword}") as conn:
+    with conn.cursor() as cur:
+        with sync_playwright() as p:
+            browser = p.chromium.connect_over_cdp(endpoint_url)
+            page = browser.contexts[0].pages[0]
+            page.goto("https://www.yankeecandle.com/yankee-candle/candles/")
 
-with sync_playwright() as p:
-    browser = p.chromium.connect_over_cdp(endpoint_url)
-    page = browser.contexts[0].pages[0]
-    page.goto("https://www.yankeecandle.com/yankee-candle/candles/")
+            def getLinks():
+                candlesContainer = page.locator(".sf-product-list-page")
 
-    def getLinks():
-        candlesContainer = page.locator(".sf-product-list-page")
+                candleTiles = candlesContainer.locator(".product-tile-container").locator("a").all()
 
-        candleTiles = candlesContainer.locator(".product-tile-container").locator("a").all()
+                links = []
 
-        links = []
+                for tile in candleTiles:
+                    link = tile.get_attribute("href")
+                    links.append(link)
 
-        for tile in candleTiles:
-            link = tile.get_attribute("href")
-            links.append(link)
-
-        print(links)
-        getCandleData(links=links)
+                print(links)
+                getCandleData(links=links)
 
 
-    def getCandleData(links):
-        for link in links:
-            newPage = browser.contexts[0].new_page()
+            def getCandleData(links):
+                for link in links:
+                    newPage = browser.contexts[0].new_page()
 
-            newPage.wait_for_timeout(200)
+                    newPage.wait_for_timeout(200)
 
-            print(link)
+                    print(link)
 
-            newPage.goto(f"https://www.yankeecandle.com{link}")
+                    newPage.goto(f"https://www.yankeecandle.com{link}")
 
-            candleName = newPage.locator(".chakra-heading").first.inner_text()
+                    candleName = newPage.locator(".chakra-heading").first.inner_text()
 
-            fragranceTextArray = newPage.locator('[class*="1mhvfii"] div.css-0').evaluate_all("""
-                elements => elements.map(el => {
-                return Array.from(el.childNodes)
-                .filter(node => node.nodeType === Node.TEXT_NODE)
-                .map(node => node.textContent.trim())
-            })
-            """)
+                    fragranceTextArray = newPage.locator('[class*="1mhvfii"] div.css-0').evaluate_all("""
+                        elements => elements.map(el => {
+                        return Array.from(el.childNodes)
+                        .filter(node => node.nodeType === Node.TEXT_NODE)
+                        .map(node => node.textContent.trim())
+                    })
+                    """)
 
-            fragranceText = []
+                    fragranceText = []
 
-            for i in range(len(fragranceTextArray)):
-                split = fragranceTextArray[i][0].split(",")
-                for j in range(len(split)):
-                    fragranceText.append(split[j].strip())
+                    for i in range(len(fragranceTextArray)):
+                        split = fragranceTextArray[i][0].split(",")
+                        for j in range(len(split)):
+                            fragranceText.append(split[j].strip())
 
-            print(fragranceText)
-            
+                    print(fragranceText)
+
+
+                #def saveCandleData(candleName, fragrances):
+                    
+
 
     getLinks()
