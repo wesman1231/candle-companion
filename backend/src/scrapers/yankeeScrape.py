e084ef143df4659f651679d4ea1a437649b2e007
Wesley <wesley@DESKTOP-KRIS84H.localdomain>
2026-09-24 19:17:56 -0700

    refactored for readability, now uploads candle data to database

--- backend/src/scrapers/yankeeScrape.py
+++ backend/src/scrapers/yankeeScrape.py
@@ -10,6 +10,8 @@ dbName = os.getenv("dbName")
 dbUser = os.getenv("dbUser")
 dbPassword = os.getenv("dbPassword")
 
+print(f"Connecting to database: '{dbName}' as user: '{dbUser}'")
+
 links = []
 
 def getLinks():
@@ -22,50 +24,85 @@ def getLinks():
         links.append(link)
 
     print(links)
+    return links
 
-def getCandleData(links):
-    for link in links:
-        newPage = browser.contexts[0].new_page()
 
-        newPage.wait_for_timeout(200)
+def getCandleName(page):
+    candleName = page.locator(".chakra-heading").first.inner_text()
+    print(candleName)
+    return candleName
 
-        print(link)
 
-        newPage.goto(f"https://www.yankeecandle.com{link}")
+def getCandleImage(page):
+    candleImage = page.get_attribute('[data-testid="product-image"]', "src")
+    print(candleImage)
+    return candleImage
 
-        candleName = newPage.locator(".chakra-heading").first.inner_text()
 
-        candleImage = newPage.get_attribute('[data-testid="product-image"]', "src")
+def getCandleFragrances(page):
+    fragranceTextArray = page.locator('[class*="1mhvfii"] div.css-0').evaluate_all("""
+                elements => elements.map(el => {
+                return Array.from(el.childNodes)
+                .filter(node => node.nodeType === Node.TEXT_NODE)
+                .map(node => node.textContent.trim())
+            })
+            """)
 
-        fragranceTextArray = newPage.locator('[class*="1mhvfii"] div.css-0').evaluate_all("""
-            elements => elements.map(el => {
-            return Array.from(el.childNodes)
-            .filter(node => node.nodeType === Node.TEXT_NODE)
-            .map(node => node.textContent.trim())
-        })
-        """)
-
-        fragranceText = []
-
-        for i in range(len(fragranceTextArray)):
+    fragranceText = []
+    
+    for i in range(len(fragranceTextArray)):
             split = fragranceTextArray[i][0].split(",")
             for j in range(len(split)):
                 fragranceText.append(split[j].strip())
 
-        print(candleImage, fragranceText)
+    print(fragranceText)
+    return fragranceText
+
+     
+def saveCandleData(candleName, candleThumbnail, fragrances):
+    with conn.transaction():
+        insertCandle = ("""
+            INSERT INTO candles (candle_name, candle_thumbnail) VALUES (%s, %s)
+            RETURNING candle_id;
+        """)
+
+        cur.execute(insertCandle, (candleName, candleThumbnail))
+
+        candleId = cur.fetchone()[0]
+
+        insertFragrance = """
+                INSERT INTO fragrances (fragrance_id, fragrance_name) VALUES (%s, %s)
+                """
+
+        for fragrance in fragrances:
+            cur.execute(insertFragrance, (candleId, fragrance))
+
+
+def scrapeCandles(links):
+    for link in links:
+        newPage = browser.contexts[0].new_page()
+        newPage.wait_for_timeout(200)
+        newPage.goto(f"https://www.yankeecandle.com{link}")
+
+        saveCandleData(getCandleName(newPage), getCandleImage(newPage), getCandleFragrances(newPage))
+
+
 
 
 
 sb = sb_cdp.Chrome()
 endpoint_url = sb.get_endpoint_url()
-with psycopg.connect(f"host=localhost dbname={dbName} user={dbUser} password={dbPassword}") as conn:
+with psycopg.connect(f"host=127.0.0.1 dbname={dbName} user={dbUser} password={dbPassword}") as conn:
     with conn.cursor() as cur:
+        cur.execute("SELECT inet_server_port(), current_database(), pg_backend_pid();")
+        print("Python is connected to:", cur.fetchone())
+        
+        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+        print("Tables Python can see:", cur.fetchall())
         with sync_playwright() as p:
             browser = p.chromium.connect_over_cdp(endpoint_url)
             page = browser.contexts[0].pages[0]
             page.goto("https://www.yankeecandle.com/yankee-candle/candles/")
 
             getLinks()
-            getCandleData(links)
-            
-            #def saveCandleData(candleName, fragrances):
+            scrapeCandles(links)
